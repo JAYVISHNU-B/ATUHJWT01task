ed73dbd1616c7b87bd0911384f730fe90b8481b2
JAYVISHNU-B <jayvishnubalaji352@gmail.com>
2024-10-29 10:51:08 +0530

    commit updated

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authroutes from "./routes/routes.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome To Our Api");
+});
 
 app.use('/api/auth', authroutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.port || 4000;
+
+app.listen(port, () => {
+  console.log("Server Started and running on the port");
+});
